b0dd96d22d7699700779559db5fd7db26ceb5494
Kay Gonschior <kay.gonschior@googlemail.com>
2016-02-20 15:14:17 +0100

    add SpriteVBO

--- kgEngine-2.0/TileMapGame/VBO.h
+++ kgEngine-2.0/TileMapGame/VBO.h
@@ -35,5 +35,30 @@ namespace kg
 		bool isGenerated()const;
 		bool isBound()const;
 		bool isMapped()const;
+
+		void draw( const RenderStates& states, std::size_t vertexCount );
+	};
+
+	class SpriteVBO : public VBO
+	{
+		static const sf::Uint8 m_vertexCountPerSprite = 4;
+		std::vector<sf::Sprite*> m_sprites;
+
+		sf::Texture* texture = nullptr;
+
+		void push_back_sprite( const sf::Sprite* sprite );// buffer has to be bound and mapped
+
+		void recreateChache();
+		void chacheSprite( const sf::Sprite& sprite );
+	public:
+		SpriteVBO();
+		SpriteVBO( unsigned int vertexCapacity, GLenum usage );
+		~SpriteVBO() = default;
+
+		void addSprites( const std::vector<sf::Sprite*> sprites );
+		void removeSprites( const std::vector<sf::Sprite*> sprites );
+		void clear();
+
+		void draw( const RenderStates& states );
 	};
 }
