1fe26f5747fcdddef01bdaa5f6a051b688b30a51
Kay Gonschior <kay.gonschior@googlemail.com>
2016-05-07 15:56:51 +0200

    cmake copy TileMapGame build files to project directory

--- Project/build.bat
+++ Project/build.bat
@@ -1,26 +1,7 @@
 ::boost
 ::MANUALLY
 
-::SFML
-cmake SFML/CMakeLists.txt -G"Visual Studio 14 2015"
-cmake --build SFML --config Debug
-cmake --build SFML --config Release
-
-::prevent SFML not being found by cmake find_sfml()
-XCOPY "./SFML/lib/Debug" "./SFML/lib" /Y
-XCOPY "./SFML/lib/Release" "./SFML/lib" /Y
-
-::Thor
-cmake Thor/CMakeLists.txt -G"Visual Studio 14 2015" -DSFML_ROOT="../SFML"
-cmake --build Thor --config Debug
-cmake --build Thor --config Release
-
-::TGUI
-cmake TGUI/CMakeLists.txt -G"Visual Studio 14 2015" -DSFML_ROOT="../SFML"
-cmake --build TGUI --config Debug
-cmake --build TGUI --config Release
-
-::NewBit
-cmake NewBit/CMakeLists.txt -G"Visual Studio 14 2015"
-cmake --build NewBit --config Debug
-cmake --build NewBit --config Release
+delete_cache.bat
+build_dependencies.bat
+install_dependencies.bat
+build_project.bat
